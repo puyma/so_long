826eee31177a41d11860a612104a911b7553c29e
puyma <mpuig-ma@student.42barcelona.com>
2022-10-08 11:46:29 +0200

    init header

---
+++ src/ft_so_long.h
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_so_long.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/08 11:20:32 by mpuig-ma          #+#    #+#             */
+/*   Updated: 2022/10/08 11:29:34 by mpuig-ma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h> /* write, close, read */
+#include <fcntl.h> /* open */
+#include <stdlib.h> /* malloc, free, exit */
+#include <stdio.h> /* perror */
+#include <string.h> /* strerror */
