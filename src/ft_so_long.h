f62d0677decd1052bcd38e342fc77a035894346c
puyma <mpuig-ma@student.42barcelona.com>
2022-10-10 18:59:15 +0200

    header protection

--- src/ft_so_long.h
+++ src/ft_so_long.h
@@ -6,12 +6,23 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/08 11:20:32 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2022/10/08 11:29:34 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2022/10/10 18:55:59 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h> /* write, close, read */
-#include <fcntl.h> /* open */
-#include <stdlib.h> /* malloc, free, exit */
-#include <stdio.h> /* perror */
-#include <string.h> /* strerror */
+#ifndef FT_SO_LONG_H
+# define FT_SO_LONG_H
+
+# include <unistd.h> /* write, close, read */
+# include <fcntl.h> /* open */
+# include <stdlib.h> /* malloc, free, exit */
+# include <stdio.h> /* perror */
+# include <string.h> /* strerror */
+
+# include "../libft/src/libft.h"
+
+int	ft_isextension(const char *str, const char *extension);
+int	ft_launch(const char *input);
+int	ft_check_map(int fd);
+
+#endif /* ft_so_long.h */
