3cdb0f08c3afc57abdefa14aef932cd888fdf016
puyma <mpuig-ma@student.42barcelona.com>
2022-10-10 18:58:28 +0200

    only and basic main

--- src/ft_so_long.c
+++ src/ft_so_long.c
@@ -6,25 +6,26 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 15:36:50 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2022/10/07 21:42:53 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2022/10/10 18:56:13 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/src/libft.h"
-#include <unistd.h>
-#include <stdio.h>
-
-int	ft_isextension(const char *str, const char *extension)
-{
-	return (1);
-	return (0);
-}
+#include "ft_so_long.h"
 
 int	main(int argc, char **argv)
 {
-	if (argc == 2 && ft_isextension(argv[1], ".ber") != 0)
+	const char	*input;
+	const char	*default_map = "maps/default.ber";
+
+	if (argc == 1)
+		ft_launch(default_map);
+	else if (argc == 2)
 	{
-		write(1, "We are all in this together\n", 29);
+		input = argv[1];
+		if (*input == '\0' || ft_isextension(input, ".ber") != 0)
+			return (0);
+		ft_launch(input);
 	}
 	return (0);
 }
