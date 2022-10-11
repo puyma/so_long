758cd866e2cec35c4f315e890fa8a91dbfa66979
puyma <mpuig-ma@student.42barcelona.com>
2022-10-11 18:06:27 +0200

    redrum

--- src/ft_launch.c
+++ src/ft_launch.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 17:10:16 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2022/10/11 17:26:17 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2022/10/11 18:04:58 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,50 @@
 #define WIDTH 1080
 #define HEIGHT 720
 
+typedef struct	s_data
+{
+	void	*img;
+	char	*address;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}			t_data;
+
+void	ft_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	int offset = (y * data->line_length + x * (data->bits_per_pixel / 8));
+	dst = data->address + offset;
+	*(unsigned int*)dst = color;
+}
+
 int	ft_launch_game(void)
 {
 	void	*mlx;
 	void	*mlx_window;
+	t_data	img;
 
 	mlx = mlx_init();
 	mlx_window = mlx_new_window(mlx, WIDTH, HEIGHT, "TDB");
+
+	img.img = mlx_new_image(mlx, WIDTH, HEIGHT);
+	img.address = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+
+	int x = 0;
+	int y = 0;
+	while (x < WIDTH)
+	{
+		y = 0;
+		while (y < HEIGHT)
+		{
+			ft_mlx_pixel_put(&img, x, y,0x00FF0000);
+			y++;
+		}
+		x++;
+	}
+
+	mlx_put_image_to_window(mlx, mlx_window, img.img, 0, 0);
 	mlx_loop(mlx);
 	return (0);
 }
