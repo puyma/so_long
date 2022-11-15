80487ea86282939d1e3233ecb12fc46bb229d422
puyma <mpuig-ma@student.42barcelona.com>
2022-11-15 13:31:28 +0100

    purged old stuff

--- src/ft_launch_graphics.c
+++ src/ft_launch_graphics.c
@@ -12,12 +12,13 @@
 
 #include "ft_so_long.h"
 
+/*
 int	ft_load_button(t_button *b, t_imgdata *img)
 {
 	ft_draw_rectangle(b->width, b->height, b->x, b->y, img);
 	return (0);
 }
-
+*/
 /*
 int	ft_attach_event_to_button(t_button *b, int (*func)(void), t_game *game)
 {
@@ -33,37 +34,11 @@ int	ft_launch_graphics(void)
 	t_imgdata	img;
 
 	game.size = PIX_SIZE;
-
+	game.height = PIX_SIZE * 10;
+	game.width = PIX_SIZE * 10;
 	game.mlx = mlx_init();
-	game.mlx_window = mlx_new_window(game.mlx, DEF_WIDTH, DEF_HEIGHT, "TDB");
-
-	//img.img = mlx_new_image(game.mlx, DEF_WIDTH, DEF_HEIGHT);
-	//img.address = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-
-	/*
-	t_button *start_button;
-	start_button = ft_new_button(200, 200, 300, 300);
-	start_button->click_event = ft_test;
-	t_list *node = ft_lstnew(start_button); // t_button should go inside t_list->content
-	game.buttons = &node;
-
-	t_button *secondary_button;
-	secondary_button = ft_new_button(42, 42, 200, 340);
-	node = ft_lstnew(secondary_button);
-	ft_lstadd_back(game.buttons, node);
-	*/
-
-	/*
-	ft_attach_event_to_button(start_button, ft_test, &game);
-	*/
-
-	/*
-	ft_load_button(start_button, &img);
-	ft_load_button(secondary_button, &img);
-	*/
-	
-	img.img = (void *) malloc(sizeof(void *) * 2);
-	img.img = mlx_xpm_file_to_image(game.mlx, "./src/assets/suelo.xpm", &img.width, &img.height);
+	game.mlx_window = mlx_new_window(game.mlx, game.width, game.height, "TDB");
+	img.img = mlx_xpm_file_to_image(game.mlx, FLOOR, &img.width, &img.height);
 	if (img.img == NULL)
 	{
 		ft_putstr("Error: ");
@@ -71,11 +46,12 @@ int	ft_launch_graphics(void)
 		return (79);
 	}
 	else
-	{
-		//img.address = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
 		mlx_put_image_to_window(game.mlx, game.mlx_window, img.img, 0, 0);
-	}
 	ft_set_events(&game);
 	mlx_loop(game.mlx);
 	return (0);
 }
+
+//img.img = mlx_new_image(game.mlx, DEF_WIDTH, DEF_HEIGHT);
+//img.address = mlx_get_data_addr(img.img, &img.bits_per_pixel, 
+//&img.line_length, &img.endian);
