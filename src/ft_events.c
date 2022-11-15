e2da75c93250c7977f84490c4c5f9bd0506e27ab
puyma <mpuig-ma@student.42barcelona.com>
2022-11-15 13:28:03 +0100

    rm unused code

--- src/ft_events.c
+++ src/ft_events.c
@@ -18,6 +18,7 @@ int	ft_destroy(t_game *game)
 	exit(0);
 	return (0);
 }
+
 int	ft_keycode(int keycode, t_game *game)
 {
 	(void) game;
@@ -29,13 +30,6 @@ int	ft_keycode(int keycode, t_game *game)
 	return (0);
 }
 
-int	ft_nothing(int x, int y, t_game *game)
-{
-	(void) game;
-	ft_printf("mousemove> : %d, Y: %d\n", x, y);
-	return (0);
-}
-
 int	ft_mousecode(int button, int x, int y, t_game *game)
 {
 	ft_printf("button: %d, x: %d, y: %d\n", button, x, y);
@@ -43,12 +37,18 @@ int	ft_mousecode(int button, int x, int y, t_game *game)
 	return (0);
 }
 
-int	ft_test(void)
+int	ft_set_events(t_game *game)
 {
-	write(1, "This actually worked\n", 21);
+	mlx_key_hook(game->mlx_window, &ft_keycode, game);
+	mlx_mouse_hook(game->mlx_window, &ft_mousecode, game);
+	mlx_hook(game->mlx_window, 17, 0, &ft_destroy, game);
 	return (0);
 }
 
+//mlx_loop_hook(game->mlx, &ft_nothing, game);
+//mlx_hook(game->mlx_window, 6, 0, &ft_do_motion_events, game);
+
+/*
 int	ft_do_motion_events(int x, int y, t_game *game)
 {
 	t_list		*button_node;
@@ -63,24 +63,10 @@ int	ft_do_motion_events(int x, int y, t_game *game)
 		{
 			//if (button->y <= y && y <= button->y + button->height)
 			(void) y;
-				write(1, "Clickitty\n", 10);
+			write(1, "Clickitty\n", 10);
 		}
 		button_node = button_node->next;
 	}
-
 	return (0);
 }
-
-/*
- * function pointer :)
 */
-
-int	ft_set_events(t_game *game)
-{
-	//mlx_loop_hook(game->mlx, &ft_nothing, game);
-	mlx_key_hook(game->mlx_window, &ft_keycode, game);
-	mlx_mouse_hook(game->mlx_window, &ft_mousecode, game);
-	//mlx_hook(game->mlx_window, 6, 0, &ft_do_motion_events, game); //motionotify
-	mlx_hook(game->mlx_window, 17, 0, &ft_destroy, game);
-	return (0);
-}
