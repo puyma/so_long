7e59c80563fb5df0dab68d1e9d72f351ed663509
puyma <mpuig-ma@student.42barcelona.com>
2023-02-13 18:49:37 +0100

    bypassed for now

--- src/ft_map_path.c
+++ src/ft_map_path.c
@@ -6,16 +6,25 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 18:21:42 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/02/13 16:08:50 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/02/13 18:19:19 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_so_long.h"
+
+/*
+ * eenum 
+ *     If dir=0 try Up direction.
+    If dir=1 try left direction.
+    If dir=2 try down direction.
+    If dir=3 try right direction.
+*/
 
 int	ft_path_isvalid(t_map *map)
 {
 	ft_printf("> checking path...\n");
 	(void) map;
 	map->exit_str = "Path is invalid";
+	return (1);
 	return (0);
 }
