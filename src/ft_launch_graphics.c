2ff062fd4c0db6b5a4ad5f43d007e2aedde67de2
puyma <mpuig-ma@student.42barcelona.com>
2023-02-13 19:16:36 +0100

    img loading fn in one file

--- src/ft_launch_graphics.c
+++ src/ft_launch_graphics.c
@@ -6,11 +6,14 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 19:20:15 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/02/13 18:47:28 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/02/13 19:12:01 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_so_long.h"
+
+int	ft_preload_img(t_game *game);
+int	ft_load_img(t_game *game);
 
 int	ft_launch_graphics(t_map *map)
 {
@@ -21,7 +24,9 @@ int	ft_launch_graphics(t_map *map)
 	game.width = map->lnlen * game.size;
 	game.height = map->lstsize * game.size;
 	game.mlx = mlx_init();
-	game.mlx_window = mlx_new_window(game.mlx, game.width, game.height, "so_longgy");
+	game.mlx_window = mlx_new_window(game.mlx, game.width, game.height, "so_long");
+	if (ft_preload_img(&game) != 0)
+		ft_load_img(&game);
 	img.img = mlx_xpm_file_to_image(game.mlx, FLOOR, &img.width, &img.height);
 	if (img.img == NULL)
 		ft_exit_str("Could not load image", 79);
@@ -30,4 +35,16 @@ int	ft_launch_graphics(t_map *map)
 	ft_set_events(&game);
 	mlx_loop(game.mlx);
 	return (0);
+}
+
+int	ft_preload_img(t_game *game)
+{
+	(void) game;
+	return (1);
+}
+
+int	ft_load_img(t_game *game)
+{
+	(void) game;
+	return (1);
 }
