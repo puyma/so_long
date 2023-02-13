4269121399dbab01a250827485465173d148edc8
puyma <mpuig-ma@student.42barcelona.com>
2023-02-13 16:11:42 +0100

    surround walls check

--- src/ft_map_content.c
+++ src/ft_map_content.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 19:22:14 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/02/10 14:05:54 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/02/13 15:09:29 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int	ft_check_characters(t_map *map)
 		temp = t->content;
 		while (i-- > 0)
 		{
-			if (ft_strchr(ALLOWED_CHARS, *temp) != 0)
+			if (ft_strchr(ALLOWED_CHARS, *temp) == 0)
 			{
 				map->exit_str = "Invalid character found in map.";
 				return (0);
@@ -43,6 +43,37 @@ int	ft_check_characters(t_map *map)
 
 int	ft_check_surroundings(t_map *map)
 {
-	(void) map;
+	t_list			*t;
+	char			*temp;
+	unsigned int	line;
+	unsigned int	i;
+
+	t = map->lst;
+	temp = NULL;
+	line = 0;
+	while (t != NULL)
+	{
+		temp = t->content;
+		if (line == 0 || line == map->lstsize)
+		{
+			i = 0;
+			while (i < map->lnlen)
+			{
+				if (temp[i] != map->c_wall)
+				{
+					map->exit_str = "Map is not surrounded by walls";
+					return (0);
+				}
+				i++;
+			}
+		}
+		else if (temp[0] != map->c_wall || temp[map->lnlen - 1] != map->c_wall)
+		{
+			map->exit_str = "Map is not surrounded by walls";
+			return (0);
+		}
+		line++;
+		t = t->next;
+	}
 	return (1);
 }
