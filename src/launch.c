e3b2f4e02c9cd7170f858e86c1e7130f49b53c4c
puyma <mpuig-ma@student.42barcelona.com>
2023-02-16 12:37:32 +0100

    map init

--- src/launch.c
+++ src/launch.c
@@ -6,21 +6,19 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 17:10:16 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/02/14 16:56:55 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/02/16 11:36:11 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+static int	ft_init_map(t_map *map);
+
 int	ft_launch(const char *input_file)
 {
 	t_map	map;
 
-	map.c_floor = C_EMPTY_SPACE;
-	map.c_wall = C_WALL;
-	map.c_collectible = C_COLLECTIBLE;
-	map.c_exit = C_EXIT;
-	map.c_player = C_PLAYER;
+	ft_init_map(&map);
 	map.filename = (char *) input_file;
 	if (ft_map_isvalid(&map))
 	{
@@ -29,5 +27,15 @@ int	ft_launch(const char *input_file)
 	}
 	else
 		ft_exit_str(map.exit_str, -1);
+	return (0);
+}
+
+static int	ft_init_map(t_map *map)
+{
+	map->c_floor = C_EMPTY_SPACE;
+	map->c_wall = C_WALL;
+	map->c_collectible = C_COLLECTIBLE;
+	map->c_exit = C_EXIT;
+	map->c_player = C_PLAYER;
 	return (0);
 }
