d3e425df626e11ff7e8ba603f5ea4bb2b70c2e5d
puyma <mpuig-ma@student.42barcelona.com>
2023-02-23 18:53:37 +0100

    simple log

--- src/log.c
+++ src/log.c
@@ -12,6 +12,12 @@
 
 #include "so_long.h"
 
+void	ft_log(char *str)
+{
+	while (*str != '\0')
+		write(1, str++, 1);
+}
+
 void	ft_log_state(t_game *game)
 {
 	ft_printf("> state [%d]\n", game->state);
