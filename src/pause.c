1317856481e715e962656033b96241b71ce22b85
puyma <mpuig-ma@student.42barcelona.com>
2023-02-23 18:52:39 +0100

    rm putstr for ft_log

--- src/pause.c
+++ src/pause.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 11:28:03 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/02/22 11:28:05 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/02/23 17:31:24 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	ft_toggle_pause(t_game *game)
 		ft_put_images(game);
 		game->state = Running;
 	}
-	ft_putstr("> Pause\n");
+	ft_log("> Pause\n");
 	ft_log_state(game);
 	return (0);
 }
