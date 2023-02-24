38eeb99c4f1378a9eb188ddab3d40a4cb9d4cbbb
puyma <mpuig-ma@student.42barcelona.com>
2023-02-24 09:32:39 +0100

    ft_set_direction to comply w/ norminette

--- src/move.c
+++ src/move.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 17:00:55 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/02/23 18:39:34 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/02/24 09:18:48 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 static int	ft_slide(t_game *game, t_character *player, t_vector *direction);
 static int	ft_ismovable(t_game *game, t_character *character,
 				t_vector *d, int keycode);
+static int	ft_set_direction(enum e_character move, t_vector *d);
 
 int	ft_move(t_game *game, t_character *player, int keycode)
 {
@@ -66,18 +67,9 @@ static int	ft_ismovable(t_game *game, t_character *character,
 	int					cy;
 
 	move = ft_ismovekey(keycode);
-	d->x = 0;
-	d->y = 0;
 	if (move == None)
 		return (0);
-	if (move == Left)
-		d->y = -1;
-	else if (move == Right)
-		d->y = 1;
-	else if (move == Up)
-		d->x = -1;
-	else if (move == Down)
-		d->x = 1;
+	ft_set_direction(move, d);
 	cx = character->x + d->x;
 	cy = character->y + d->y;
 	if (game->map->arr[cx][cy].c == '1')
@@ -86,7 +78,6 @@ static int	ft_ismovable(t_game *game, t_character *character,
 		game->map->n_collectible--;
 	else if (game->map->arr[cx][cy].c == 'E' && game->map->n_collectible == 0)
 		game->state = Stopping;
-	ft_printf(">>n_collectible: %d", game->map->n_collectible);
 	return (move);
 }
 
@@ -104,4 +95,19 @@ int	ft_ismovekey(int keycode)
 	else if (keycode == KEY_LEFT || keycode == KEY_A)
 		direction = Left;
 	return (direction);
+}
+
+static int	ft_set_direction(enum e_character move, t_vector *d)
+{
+	d->x = 0;
+	d->y = 0;
+	if (move == Left)
+		d->y = -1;
+	else if (move == Right)
+		d->y = 1;
+	else if (move == Up)
+		d->x = -1;
+	else if (move == Down)
+		d->x = 1;
+	return (move);
 }
