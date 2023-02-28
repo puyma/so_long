dd306b5933783bc2016e2f7b69be1826e1ed26e3
puyma <mpuig-ma@student.42barcelona.com>
2023-02-28 10:33:52 +0100

    simplified ft_exit

--- src/ft_exit.c
+++ src/ft_exit.c
@@ -12,19 +12,10 @@
 
 #include "so_long.h"
 
-void	ft_exit_error(int error_num)
+void	ft_exit(char *error_str, int error_num)
 {
-	ft_putstr_fd("Error: ", 1);
-	ft_putstr_fd(strerror(error_num), 1);
-	ft_putstr_fd(" [", 1);
-	ft_putnbr_fd(error_num, 1);
-	ft_putstr_fd("]", 1);
-	ft_putchar_fd('\n', 1);
-	exit(error_num);
-}
-
-void	ft_exit_str(char *error_str, int error_num)
-{
+	if (*error_str == '\0')
+		error_str = strerror(error_num);
 	ft_putstr_fd("Error: ", 1);
 	ft_putstr_fd(error_str, 1);
 	ft_putstr_fd(" [", 1);
