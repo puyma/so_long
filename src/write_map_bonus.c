ed0938e7fba250a607c6373b6b343d6761166fc3
puyma <mpuig-ma@student.42barcelona.com>
2023-03-11 00:43:52 +0100

    write empty map; to be redo

--- src/write_map_bonus.c
+++ src/write_map_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 15:33:45 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/03/10 17:55:04 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/03/11 00:42:12 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,37 @@
 
 // exits to manage here (return edit)
 
+// this function should generate an empty int **board 
+// and pass it to ft_write_map();
+
 int	ft_write_empty_map(char *filename, int x, int y)
 {
+	const int	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0666);
+
 	if (x < 1 || y < 1)
 	{
-		ft_printf("usage: ./bonus file\n");
+		ft_printf("usage ./generator file [width > 0] [height > 0]\n");
 		exit(0);
 	}
-	(void) filename;
+	x += 2;
+	y += 2;
+	xx = 0;
+	while (xx++ < x)
+	{
+		yy = 0;
+		while (yy < y)
+		{
+			if (yy == 0 || yy == y - 1 || xx == 1 || xx == x)
+				write(fd, "1", 1);
+			else
+				write(fd, "0", 1);
+			if (xx == 2 && yy == 0 && y > 4)
+				yy += write(fd, "ECP", 3);
+			yy++;
+		}
+		write(fd, "\n", 1);
+	}
+	ft_putchar_fd(0, fd);
 	return (0);
 }
 
