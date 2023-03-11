46a84346e59c041f9bc1e69550637df0fa1851f8
puyma <mpuig-ma@student.42barcelona.com>
2023-03-11 22:33:20 +0100

    fix leaks ismovekey (direction)

--- src/move.c
+++ src/move.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 15:52:46 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/03/10 18:16:53 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/03/11 22:32:56 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,5 +68,10 @@ t_vector	*ft_ismovekey(int keycode)
 		direction->y = 1;
 	else if (keycode == KEY_LEFT || keycode == KEY_A)
 		direction->y = -1;
+	if (direction->x == 0 && direction->y == 0)
+	{
+		free(direction);
+		direction = NULL;
+	}
 	return (direction);
 }
