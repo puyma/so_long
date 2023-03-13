93d2d6a6fa447eccd63f558cb1b9fe262e0ee293
puyma <mpuig-ma@student.42barcelona.com>
2023-03-13 15:47:44 +0100

    corrected typo

--- src/launch.c
+++ src/launch.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 15:26:29 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/03/11 23:20:12 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/03/13 15:46:41 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	ft_launch(t_game *game)
 {
 	if (ft_memload_images(game) == 0)
-		ft_exit("Could allocate memory", 8, NULL, NULL);
+		ft_exit("Could not allocate memory", 8, NULL, NULL);
 	ft_put_images(game);
 	mlx_hook(game->mlx_window, ON_DESTROY, 0, &ft_destroy, game);
 	mlx_hook(game->mlx_window, ON_KEYDOWN, 0, &ft_keycode, game);
--- src/launch_bonus.c
+++ src/launch_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 15:27:40 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/03/13 09:01:42 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/03/13 15:46:26 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	ft_launch(t_game *game)
 {
 	if (ft_memload_images(game) == 0)
-		ft_exit("Could allocate memory", 8, NULL, NULL);
+		ft_exit("Could not allocate memory", 8, NULL, NULL);
 	ft_put_images(game);
 	mlx_hook(game->mlx_window, ON_DESTROY, 0, &ft_destroy, game);
 	mlx_hook(game->mlx_window, ON_KEYDOWN, 0, &ft_keycode, game);
